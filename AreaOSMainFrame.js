027f3b0962a312424b788ba44cfefe556ac1ed56
jojoghaida <josephghaida@gmail.com>
2017-09-05 23:11:43 -0400

    dsjflsdldk

--- AreaOSMainFrame.js
+++ AreaOSMainFrame.js
@@ -201,6 +201,7 @@ zoomInButton = function(){
       if(force>.10){
         zMinusEffet.style.visibility = 'visible';
         console.log(zMinusEffet);
+        zMinusEffet.style.borderWidth = force*10;
         // zMinusEffet.style;
       }else {
         zMinusEffet.style.visibility = 'hidden';
