9092704cd25addc40f10b159cc01686793297f52
jojoghaida <josephghaida@gmail.com>
2017-09-05 23:56:20 -0400

    m,rf gkd

--- AreaOSMainFrame.js
+++ AreaOSMainFrame.js
@@ -201,7 +201,7 @@ zoomInButton = function(){
       if(force>.10){
         zMinusEffet.style.visibility = 'visible';
         console.log(zMinusEffet);
-        zMinusEffet.style.width = force*1000 + "%";
+        zMinusEffet.style.width = force*100 + "%";
         // zMinusEffet.style;
       }else {
         zMinusEffet.style.visibility = 'hidden';
