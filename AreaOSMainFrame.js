149947f19423ad9c1e1e7889b563867c88f718f2
jojoghaida <josephghaida@gmail.com>
2017-09-25 01:58:42 -0400

    ew

--- AreaOSMainFrame.js
+++ AreaOSMainFrame.js
@@ -29,7 +29,7 @@ h = viewport.offsetHeight;
 w = viewport.offsetWidth;
 
 scene = new THREE.Scene();
-scene.background = new THREE.Color( 0x0851a4 ); //0xffffff
+scene.background = new THREE.Color(0xffffff ); //0x0851a4
 
 camera = new THREE.PerspectiveCamera( 55, w/h, 0.1, 10000 );
 //camera = new THREE.OrthographicCamera( w/2,w/-2,h/-2,h/2,1,500 );
