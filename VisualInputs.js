e575026cf5cd8de5a7ccce4f6c97c7a3a86fd746
jojoghaida <josephghaida@gmail.com>
2017-09-25 15:33:37 -0400

    hrthrt

--- VisualInputs.js
+++ VisualInputs.js
@@ -8,7 +8,7 @@ var params = {width: sW, height: sH};
 var two = new Two(params).appendTo(inputsTwoScene);
 
 
-inputsTwoScene.addEventListener('mousemove',disableOrbitCam);
+inputsTwoScene.addEventListener('mousemove',disableOrbitCam; alert());
 inputsTwoScene.addEventListener('touchstart',disableOrbitCam);
 
 //standard input
@@ -41,3 +41,42 @@ for(i=0;i<tickQuan;i++){
 }
 
 two.update();
+
+
+// document.addEventListener('touchstart', handleTouchStart, false);
+// document.addEventListener('touchmove', handleTouchMove, false);
+//
+// var xDown = null;
+// var yDown = null;
+//
+// function handleTouchStart(evt) {
+//     xDown = evt.touches[0].clientX;
+//     yDown = evt.touches[0].clientY;
+// };
+//
+// function handleTouchMove(evt) {
+//     if ( ! xDown || ! yDown ) {
+//         return;
+//     }
+//
+//     var xUp = evt.touches[0].clientX;
+//     var yUp = evt.touches[0].clientY;
+//
+//     var xDiff = xDown - xUp;
+//     var yDiff = yDown - yUp;
+//
+//     if ( Math.abs( xDiff ) > Math.abs( yDiff ) ) {/*most significant*/
+//         if ( xDiff > 0 ) {
+//             /* left swipe */
+//         } else {
+//             /* right swipe */
+//         }
+//     } else {
+//         if ( yDiff > 0 ) {
+//             /* up swipe */
+//         } else {
+//             /* down swipe */
+//         }
+//     }
+//     /* reset values */
+//     xDown = null;
