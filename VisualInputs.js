e0ee085909b4f7a60206c86410e470e4d3b5320b
jojoghaida <josephghaida@gmail.com>
2017-09-25 03:49:58 -0400

    123

--- VisualInputs.js
+++ VisualInputs.js
@@ -1,16 +1,18 @@
 //two js form inputs
 
+//setting canvas
 var inputsTwoScene = document.getElementById('inputCanvas');
 var sW = inputsTwoScene.offsetWidth
 var sH = inputsTwoScene.offsetHeight
 var params = {width: sW, height: sH};
-
 var two = new Two(params).appendTo(inputsTwoScene);
 
-var rectangle = two.makeRectangle(0,0,sW,-sH);
-rectangle.fill = 'green';
 
-var line = two.makeLine(0,sH,sW,sH);
+//standard input
+// var rectangle = two.makeRectangle(0,0,sW,-sH);
+// rectangle.fill = 'green';
+
+var line = two.makeLine(0,sH*.8,sW,sH*.8);
 line.linewidth = 3
 
 two.update();
