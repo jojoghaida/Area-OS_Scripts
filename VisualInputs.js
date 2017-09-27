43d98a963e7134db665ed04a97b1efb58c347c90
jojoghaida <josephghaida@gmail.com>
2017-09-27 04:27:57 -0400

    retertre

--- VisualInputs.js
+++ VisualInputs.js
@@ -58,7 +58,7 @@ swipeUpTxt.family = 'Karla';
 swipeUpTxt.alignment = 'center';
 
 var clrStop1 = new Two.Stop(1,'white',0);
-var clrStop2 = new Two.Stop(0,colorStyle,1);
+var clrStop2 = new Two.Stop(.1,colorStyle,1);
 var clrStop3 = new Two.Stop(.75,colorStyle,1);
 var swipeUpGradient = two.makeLinearGradient(sW,-sH/2,sW,sH/2,clrStop1,clrStop2);
 var submitRec = two.makeRectangle(sW/2,sH/2,sW,sH);
@@ -157,7 +157,7 @@ function handleTouchMove(evt) {
     } else {
         if ( yDiff > 0 ) {
             /* up swipe */
-            clrStop1.offset = (yDiff/submitThreshold)-1;
+            clrStop1.offset = yDiff/submitThreshold;
             console.log(yDiff/submitThreshold);
             two.update();
             if(yDiff>submitThreshold){ //submitThreshold<<
