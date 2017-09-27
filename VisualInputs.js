2ee651ded2b88ebca8787a34b621b21f1cf3c15f
jojoghaida <josephghaida@gmail.com>
2017-09-27 03:29:04 -0400

    ferferg

--- VisualInputs.js
+++ VisualInputs.js
@@ -51,8 +51,15 @@ swipeUpTxt.alignment = 'center';
 
 var clrStop1 = new Two.Stop(.25,'white',0);
 var clrStop2 = new Two.Stop(.75,colorStyle,1);
+var clrStop3 = new Two.Stop(.75,colorStyle,1);
+
 var swipeUpGradient = two.makeLinearGradient(sW,-sH/2,sW,sH/2,clrStop1,clrStop2);
 
+function test(){
+  clrStop2.offset = .5;
+  two.update();
+}
+setTimeout(test,1000);
 // two.width / 2, - two.height / 2, two.width / 2, two.height / 2,
 
 var submitRec = two.makeRectangle(sW/2,sH/2,sW,sH);
