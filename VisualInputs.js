ee93be0e6645887d1a6a97a208e1239b4f295333
jojoghaida <josephghaida@gmail.com>
2017-09-27 04:48:33 -0400

    dfgfdg

--- VisualInputs.js
+++ VisualInputs.js
@@ -104,7 +104,7 @@ swipeUpTxt.size = 12;
 swipeUpTxt.family = 'Karla';
 swipeUpTxt.alignment = 'center';
 
-var clrStop1 = new Two.Stop(0,'white',0);
+var clrStop1 = new Two.Stop(.9,'white',0);
 var clrStop2 = new Two.Stop(1,colorStyle,1);
 var clrStop3 = new Two.Stop(.75,colorStyle,1);
 var swipeUpGradient = two.makeLinearGradient(sW,-sH/2,sW,sH/2,clrStop1,clrStop2);
