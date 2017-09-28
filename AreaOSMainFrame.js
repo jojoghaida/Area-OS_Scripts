03fd48c512d2c24048792f65ea9884ae5c52daa2
jojoghaida <josephghaida@gmail.com>
2017-09-28 00:18:45 -0400

    gdgfttrg

--- AreaOSMainFrame.js
+++ AreaOSMainFrame.js
@@ -522,7 +522,7 @@ if(areaSQ<requestedSQ){
       // extendCrv(inputMainCrv,furnishRequest/*<fix*/,getCrvVector(inputMainCrv));
       editMainCrv = setInterval(function(){
         extendCrv(inputMainCrv,furnishRequest/*<fix*/,getCrvVector(inputMainCrv));
-        if(inputMainCrv/5/*<<<spacing tempo*/ % 1 == 0){
+        if(getCrvLength(inputMainCrv)/5/*<<<spacing tempo*/ % 1 == 0){
           console.log("little crv drop");
         }
       },10);
