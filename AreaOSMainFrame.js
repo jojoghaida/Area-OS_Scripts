42f48109f6b4cb678f34523060f7e76a595832ab
jojoghaida <josephghaida@gmail.com>
2017-10-05 15:33:35 -0400

    efwewa

--- AreaOSMainFrame.js
+++ AreaOSMainFrame.js
@@ -583,8 +583,8 @@ if(areaSQ<requestedSQ){
       }/*curve2 reduction end*/else{//second curve reduction
         extendCrv(inputSecondaryCrv,secondaryConCrvsGrothInterval,reverseUnitVector(getCrvVector(inputSecondaryCrv)));
         if(Number(getCrvLength(inputSecondaryCrv).toFixed(2))/2 /*<<<spacing tempo*/ % 1 == 0){
-          furnitureGroup.remove(furnitureGroup.children[furnitureGroup.children.length - 1]);
-          // removeElement.remove();
+          removal = furnitureGroup.remove(furnitureGroup.children[furnitureGroup.children.length - 1]);
+          scene.remove(removal);
           renderer.render(scene,camera);
         }
         if(Number(getCrvLength(inputSecondaryCrv).toFixed(2)) <= secondaryConCrvsGrothInterval){
