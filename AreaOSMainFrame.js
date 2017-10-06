b8ce07eb231ff0f5571cbfedef70995f026da25c
jojoghaida <josephghaida@gmail.com>
2017-10-06 02:40:25 -0400

    vhnjgfh

--- AreaOSMainFrame.js
+++ AreaOSMainFrame.js
@@ -22,7 +22,7 @@ aboutButton.addEventListener('click',launchAbout);
 
 //SCENE AND CONTROLS////////////////////////////////////////////////////////////
 var camera, scene, controls, renderer;
-var frustumSize = 500;
+var frustumSize = 400;
 
 viewInit();
 orbitCam();
