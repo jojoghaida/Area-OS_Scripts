eef50005475dd9968cc75136c8f6f8657e42a869
jojoghaida <josephghaida@gmail.com>
2017-10-10 21:18:02 -0400

    dawsda

--- StylesSheetAreaOS.js
+++ StylesSheetAreaOS.js
@@ -2,11 +2,11 @@
 
 //line Styles
 var aOS_LineStyles = {
-  blueLine = new THREE.LineBasicMaterial();
-  redLine = new THREE.LineBasicMaterial();
+  blueLine = new THREE.LineBasicMaterial({});
+  redLine = new THREE.LineBasicMaterial({});
 };
 
 //mesh Styles
 var aOS_MeshStyles = {
-  blueMesh = new THEE.MeshBasicMaterial();
+  blueMesh = new THEE.MeshBasicMaterial({});
 };
