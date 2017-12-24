31534f3c98b7863196f069b87eee30de50867076
jojoghaida <josephghaida@gmail.com>
2017-12-23 22:15:14 -0500

    httrhr

--- AreaOSMainFrame.js
+++ AreaOSMainFrame.js
@@ -1165,7 +1165,7 @@ function newCrawler(pt, steps = 220, init_Dir = 0){
 
   setTimeout(
     function (){
-      newCrawler(scene.getObjectByName("18,21"),350,0);
+      newCrawler(scene.getObjectByName("18,0"),350,0);
     },.1);
 
   // setTimeout(
