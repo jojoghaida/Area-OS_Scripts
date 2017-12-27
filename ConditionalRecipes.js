a9a12ac5e5d3c9851f756a6ce9bd33e4d84bf90a
jojoghaida <josephghaida@gmail.com>
2017-12-26 23:43:39 -0500

    kjhb

--- ConditionalRecipes.js
+++ ConditionalRecipes.js
@@ -1,1 +1,16 @@
-//Conditional Recipes 
+//Conditional Recipes
+var lectureCondition = {
+
+    Keynote: {
+      name: "Keynote",
+      maxTrajectory: 30,
+      minTrajectory: 5,
+    },
+
+    workshop: {
+      name: "Workshop",
+      maxTrajectory: new THREE.LineBasicMaterial({}),
+      minTrajectory: new THREE.LineBasicMaterial({}),
+    },
+
+};
