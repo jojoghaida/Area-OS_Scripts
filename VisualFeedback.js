c9b319ff4b16b46c40d17586f2616345b2c67669
jojoghaida <josephghaida@gmail.com>
2018-01-12 23:09:38 -0500

    fbdfbfd

--- VisualFeedback.js
+++ VisualFeedback.js
@@ -4,8 +4,8 @@
 var feedbackTwoScene = document.getElementById('areaoshead');
 var vfsw = feedbackTwoScene.offsetWidth
 var vfsh = feedbackTwoScene.offsetHeight
-var params = {width: vfsw, height: vfsh};
-var two = new Two(params).appendTo(feedbackTwoScene);
+var paramsToo = {width: vfsw, height: vfsh};
+var twoToo = new Two(params).appendTo(feedbackTwoScene);
 
 //mouse gestures*
 feedbackTwoScene.addEventListener('mousemove',disableOrbitCam);
