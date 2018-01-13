2b23481611b003f26d0026ce3a52ce2ee5aed1a3
jojoghaida <josephghaida@gmail.com>
2018-01-12 23:19:06 -0500

    fvdfvd

--- VisualFeedback.js
+++ VisualFeedback.js
@@ -10,10 +10,10 @@ var twoToo = new Two(params).appendTo(feedbackTwoScene);
 //mouse gestures*
 feedbackTwoScene.addEventListener('mousemove',disableOrbitCam);
 
-var titleTop = two.makeText('AREA OS',vfsw/2,15); //get glyph size to get y-value
+var titleTop = two.makeText('AREA OS',vfsw/2,vfsh/2); //get glyph size to get y-value
 titleTop.fill = colorStyle;
 titleTop.size = 24;
 titleTop.family = 'Karla';
-titleTop.alignment = 'left';
+titleTop.alignment = 'center';
 
 twoToo.update();
